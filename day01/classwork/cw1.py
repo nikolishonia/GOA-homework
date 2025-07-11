3348dad1c6e310596e6cda9cece8c49db925c021
nikolishonia <labrujainmortal666@gmail.com>
2025-07-11 22:34:25 +0400

    1

---
+++ day01/classwork/cw1.py
@@ -0,0 +1,3 @@
+print("980")
+print(66)
+print(33.60)
---
+++ day03/homework/hw3.py
@@ -0,0 +1,59 @@
+
+first_number = 94
+
+second_number = 33
+
+third_number =  first_number + second_number
+
+fourth_number = first_number - second_number
+
+fifth_number = first_number * second_number
+
+sixth_number = first_number / second_number
+
+name = "nicole"
+name = "scott"
+name = "varg"
+name = "jef"
+name = "aileen"
+name = "jodi"
+
+print(name)
+
+album = "scar sighted"
+Album = "around the fur"
+AlbuM = "filosofem"
+ALbum = "subliminal genocide"
+ALbUm = "death"
+
+print(album)
+print(Album)
+print(AlbuM)
+print(ALbum)
+print(ALbUm)
+
+
+name2 = "giorgi"
+# cvladis saxeli ar sheidzleba rom cifrit daiwyos
+username = "bubunauri"
+# { simbolo iyo aq araswori
+user_name = "goga"
+# sawiroa rom saxeli goga iyos brwyalebshi
+user_surname = "axalaia"
+# unda iyos dabali tire da ara chveulebrivi, da aseve aqac sawiroa brwyalebi
+
+color_one = "purple"
+color_two = "black"
+color_three = "pink"
+color_four = "grey"
+color_five = "blue"
+
+print(color_one + color_two + color_three + color_four + color_five)
+# stringebis gaertianebas qvia konkatinacia
+
+myname = "nicole"
+number = 10
+
+print(myname * number)
+
+#stringze da integerze ar shegvidzlia mimateba, gamokleba da gayofa, am matematikuri moqmedebebis shesrulebis shedegad gvadzlevs errors
