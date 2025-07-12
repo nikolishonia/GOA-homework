92007014b0b0d3d4d4f0c5b0e01647bd298d8ac3
nikolishonia <labrujainmortal666@gmail.com>
2025-07-12 23:24:00 +0400

    1

---
+++ day04/classwork/cw4.py
@@ -0,0 +1,30 @@
+name = input("Enter your name: ")
+
+print(name)
+
+firstnum = input("Enter a number: ")
+secondnum = input("Enter another number: ")
+
+firstnum = int(firstnum)
+secondnum = int(secondnum)
+
+print(firstnum + secondnum)
+
+age = input("Enter your age: ")
+
+
+age = int(age)
+
+print(type(age))
+
+vone = "cut"
+vtwo = 71
+vthree = 3.33
+vfour = False
+
+print(type(vone))
+print(type(vtwo))
+print(type(vthree))
+print(type(vfour))
+
+
