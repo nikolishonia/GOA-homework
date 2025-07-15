9bfd9f2ba55a29cc1e4e0710afe76b85ba7bb0d8
nikolishonia <labrujainmortal666@gmail.com>
2025-07-15 19:04:01 +0400

    1

---
+++ day04/homework/hw4.py
@@ -0,0 +1,66 @@
+# inputi aris ragac monacemis sheyvana, da outputi aris ragacis migeba shedegad 
+
+letter = input("Enter a letter: ")
+print(type(letter))
+
+color1 = "purple" #string
+color2 = "black" #string
+color3 = "pink" #string
+color4 = "grey" #string
+color5 = "white" #string
+
+firstnum = 88 #integer
+secnum = 14 #integer
+thirdnum = 90 #integer
+fourthnum = 31 #integer
+fifthnum = 7 #integer
+
+floatone = 0.33 #float
+floattwo = 4.44 #float
+floatthree = 1.88 #float
+floatfour = 2.67 #float
+floatfive = 1.64 #float
+
+song = "Contrary Pulse"
+sixthnum = 71
+floatsix = 1.11
+
+print(type(song))
+print(type(sixthnum))
+print(type(floatsix))
+
+wordone = input("Enter a word: ")
+wordtwo = input("Enter another word: ")
+
+print(wordone + wordtwo)
+
+num1 = input("First number: ")
+num2 = input("Second number: ")
+num3 = input("Third number: ")
+num4 = input("Fourth number: ")
+num5 = input("Fifth number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+num4 = int(num4)
+num5 = int(num5)
+
+print(num1 + num2 + num3 + num4 + num5 / 5)
+
+name = input("Enter your name: ")
+lastname = input("Enter your last name: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+weight = input("Enter your weight: ")
+
+print("My name is " + name + " " + lastname + ". I am " + age + " years old. I am " + height + " tall, and I weight " + weight + " kilos.")
+
+
+
+
+
+
+
+
+ 
