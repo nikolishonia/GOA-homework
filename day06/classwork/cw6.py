c59707ce169ff8c1fc76353642bc6cfd46138706
nikolishonia <labrujainmortal666@gmail.com>
2025-07-19 23:14:05 +0400

    1

---
+++ day06/classwork/cw6.py
@@ -0,0 +1,15 @@
+# logikuri operatorebi aris 
+
+name1 = input("Enter a name: ")
+name2 = "Nicole"
+
+print(name1 == name2)
+
+print(True and False)
+print(True and True)
+print(True or False)
+print(False or False)
+
+
+
+
