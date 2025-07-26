4c4f146446d669dc7f9e41f7f05dc8e21fc0a644
nikolishonia <labrujainmortal666@gmail.com>
2025-07-26 23:03:12 +0400

    1

---
+++ day08/classwork/cw8.py
@@ -0,0 +1,12 @@
+print(33 == 33 and 14 > 11)
+print(15 < 40 or 44 == 1)
+print(18 > 17 and 10 < 80)
+print(41 == 30 and 35 > 17)
+print(18 == 11 or 71 < 8)
+print(50 < 1 or 80 == 14)
+
+
+for i in range(20):
+    print("Nicole Shonia")
+
+    
