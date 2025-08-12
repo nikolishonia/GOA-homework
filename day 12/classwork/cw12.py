295bd43af334705f2008b982c6393d7be46d625c
nikolishonia <labrujainmortal666@gmail.com>
2025-08-12 05:08:18 +0400

    1

---
+++ day 12/classwork/cw12.py
@@ -0,0 +1,16 @@
+for i in range(11):
+    print(i)
+
+i = 5
+while i < 20:
+    print(i)
+    i = i + 1
+
+
+age = int(input("Age:"))
+
+if age >= 18:
+    print("You're an adult")
+else:
+    print("You're a teenager")
+
