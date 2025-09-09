183434dfb1db49a3b1c639e4c1ab603529f32184
nikolishonia <labrujainmortal666@gmail.com>
2025-09-09 12:16:05 +0400

    1

---
+++ day20/classwork/cw20.py
@@ -0,0 +1,31 @@
+# 1) კომენტარის სახით ჩამოწერეთ  რომელი ახალი მათემატიკური ოპერატორები ვისწავლეთ,თითოეულ მათგანს მიუწერეთ  რომელი რას აკეთებს და თითზე მოიყვანე სამ-სამი მაგალითი
+
+# chven viswavlet **, //, da %
+
+# ** aris ricxvis xarisxshi ayvana
+
+print(2**4) # anu 2*2*2*2
+print(3**2) # 3*3
+print(10**6) # 10*10*10*10*10*10
+
+# // aris pirvel ricxvshi meore ricxvi ramdenjer eteva
+
+print(30//20)
+print(10//7)
+print(50//2)
+
+# % aris gayofis nashti
+
+print(10%5)
+print(7%3)
+print(5%15)
+
+# 2)კომენტარის სახით ახსენიტ თუ რითი განსხვავდებიან ერთმანეთისგან სიები და ცვლადები,და ასევე დაწერე თუ რაში ვიყენებთ სიებს
+
+# cvladebshi shegvidzlia movatavsot erti monacemi da siebshi shegvidzlia ramodenime monacemebi chavsvat sxvadasxva tipebis
+
+#3)შექმენი სია რომელსაც შეინახავთ ცვლადში,ამ სიაში უნდა გქონდეს ყველა მონაცემთა ტიპის ელემენტი რომელიც ვისწავლეთ,სიის შექმნის შემდეგ დაპრინტეთ ეს სია
+
+sia = [ "xasthur" , 88 , 6.66 , True ]
+print(sia)
+
