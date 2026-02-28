5d47e45fdd50cd2f783de84fd8c03ec12501c755
nikolishonia <labrujainmortal666@gmail.com>
2026-02-28 20:49:57 +0400

    1

---
+++ day64/classwork/cw64.py
@@ -0,0 +1,19 @@
+def say_my_name():
+    print("Nicole")
+    
+say_my_name()
+
+say_my_name()
+say_my_name()
+say_my_name()
+say_my_name()
+
+
+def check_even():
+    number = 88
+    if number % 2 == 0:
+        return "this number is even"
+    else:
+        return "this number is odd"
+    
+print(check_even)
