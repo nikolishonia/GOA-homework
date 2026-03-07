b14c23444516b712b7b6a02369fbe935bd374ae1
nikolishonia <labrujainmortal666@gmail.com>
2026-03-07 21:57:33 +0400

    1

---
+++ day70/homework/hw70.py
@@ -0,0 +1,40 @@
+# 1.
+
+def greet():
+    return("hello world!")
+
+# 2.
+
+def summation(num):
+    return sum(range(1, num + 1))
+    
+    
+# 3.
+
+def sum_array(a):
+    return sum(a)
+
+
+# 4.
+
+
+# 5.
+
+def are_you_playing_banjo(name):
+    if name[0] == "R" or name[0] == "r":
+        return (name + " plays banjo")
+    else:
+        return (name + " does not play banjo")
+    
+# 6.
+
+
+# 7.
+
+def find_average(numbers):
+    if not numbers:
+        return 0
+    return sum(numbers)/len(numbers)
+
+
+# 8.
